fcbe88c4d513a0e73c6d2c262339db031ae7c396
Risto Hietala <risto.hietala@gmail.com>
2017-11-04 23:15:01 +0200

    day 5 solutions

--- src/main/scala/adventofcode2015/Day05.scala
+++ src/main/scala/adventofcode2015/Day05.scala
@@ -1,6 +1,48 @@
 package adventofcode2015
 
-object Day05 extends App {
-  lazy val greeting: String = "hello"
-  println(greeting)
+import scala.io.Source
+
+object Day05 {
+
+  val VOWELS: List[Char] = List('a', 'e', 'i', 'o', 'u')
+  val DOUBLE_CHARS: List[String] = ('a' to 'z').toList.map(c => s"${c}${c}")
+  val NOT_ALLOWED: List[String] = List("ab", "cd", "pq", "xy")
+
+  val hasThreeVowels: String => Boolean =
+    _.filter(VOWELS.contains).size >= 3
+
+  val hasSubstringInList: List[String] => String => Boolean =
+    l => s => l.exists(s.containsSlice(_))
+
+  val hasDoubleChars: String => Boolean =
+    hasSubstringInList(DOUBLE_CHARS)
+
+  val hasNotAllowed: String => Boolean =
+    hasSubstringInList(NOT_ALLOWED)
+
+  val part1: String => Boolean =
+    s => hasThreeVowels(s) && hasDoubleChars(s) && !hasNotAllowed(s)
+
+  val hasDuplicateCharPairs: String => Boolean =
+    s =>
+      (0 to (s.length - 4))
+        .map(i => s.slice(i + 2, s.length).containsSlice(s.slice(i, i + 2)))
+        .exists(_ == true)
+
+  val sameFirstAndLast: List[Char] => Boolean =
+    l => l.head == l.last
+
+  val hasThreeCharsWithSameFirstAndLast: String => Boolean =
+    s => s.toList.sliding(3).filter(_.size == 3).exists(sameFirstAndLast)
+
+  val part2: String => Boolean =
+    s => ((hasDuplicateCharPairs(s) || hasDuplicateCharPairs(s.tail)) &&
+          hasThreeCharsWithSameFirstAndLast(s))
+
+  def main(args: Array[String]): Unit = {
+    val file: String = getClass.getResource("").getPath + "day05.txt"
+    val inputs: List[String] = Source.fromFile(file).getLines.toList
+    println("Day 05 part 1: " + inputs.filter(part1).size)
+    println("Day 05 part 2: " + inputs.filter(part2).size)
+  }
 }
--- src/test/scala/adventofcode2015/Day05Spec.scala
+++ src/test/scala/adventofcode2015/Day05Spec.scala
@@ -3,7 +3,18 @@ package adventofcode2015
 import org.scalatest._
 
 class Day05Spec extends FreeSpec {
-  "The Day05 object should say hello" in {
-    assert(Day05.greeting == "hello")
+  "Part 1" - {
+    "ugknbfddgicrmopn = true" in { assert(Day05.part1("ugknbfddgicrmopn") == true) }
+    "aaa = true" in { assert(Day05.part1("aaa") == true) }
+    "jchzalrnumimnmhp = false" in { assert(Day05.part1("jchzalrnumimnmhp") == false) }
+    "haegwjzuvuyypxyu = false" in { assert(Day05.part1("haegwjzuvuyypxyu") == false) }
+    "dvszwmarrgswjxmb = false" in { assert(Day05.part1("dvszwmarrgswjxmb") == false) }
+  }
+
+  "Part 2" - {
+    "qjhvhtzxzqqjkmpb = true" in { assert(Day05.part2("qjhvhtzxzqqjkmpb") == true) }
+    "xxyxx = true" in { assert(Day05.part2("xxyxx") == true) }
+    "uurcxstgmygtbstg = false" in { assert(Day05.part2("uurcxstgmygtbstg") == false) }
+    "ieodomkazucvgmuy = false" in { assert(Day05.part2("ieodomkazucvgmuy") == false) }
   }
 }
